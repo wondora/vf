086a2a9d297ccdb20dcea7542c15e5d11eca6a92
wondora <wondora@yandex.com>
2022-01-07 16:27:35 +0900

    group

--- src/views/site/menu.vue
+++ src/views/site/menu.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
-    <v-list-item v-for="item in items" :key="item.title" :to="item.to">
-    <v-list-item-content>
-      {{ item.title}}
-    </v-list-item-content>
-  </v-list-item>
+    <v-list>
+      <v-list-group
+        v-for="(item, i) in items"
+        :key="i"
+        v-model="item.active"
+        :prepend-icon="item.icon"
+        no-action
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item
+          v-for="child in item.subItems"
+          :key="child.title"
+          :to="subItem.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="child.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
   </div>
 
 </template>
@@ -16,15 +36,28 @@ export default {
       items: [
         {
           title: 'home',
-          to: '/'
+          icon: 'mdi-home',
+          subItems: [
+            {
+              title: 'Dashboard',
+              to: '/'
+            },
+            {
+              title: 'about',
+              to: '/about'
+            }
+          ]
         },
         {
           title: 'about',
-          to: '/about'
-        },
-        {
-          title: 'xxx',
-          to: '/xxx'
+          active: true,
+          icon: 'mdi-account',
+          subItems: [
+            {
+              title: 'xxx',
+              to: '/xxx'
+            }
+          ]
         }
       ]
     }
