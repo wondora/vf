7edfcd814bce1a3219db1bfdde0ba414b11a0bb9
wondora <wondora@yandex.com>
2022-01-07 11:00:28 +0900

    navigation

--- src/App.vue
+++ src/App.vue
@@ -5,41 +5,37 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon to="/about">
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-main>
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+    </v-navigation-drawer>
+    <v-content>
       <router-view/>
-    </v-main>
+    </v-content>
+    <v-footer app color="primary" dark absolute>
+      <v-spacer/>
+      <div>
+        &copy; {{ new Date().getFullYear() }}
+      </div>
+    </v-footer>
+
   </v-app>
 </template>
 
@@ -47,9 +43,10 @@
 
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  })
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
